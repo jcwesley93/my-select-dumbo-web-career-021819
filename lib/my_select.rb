8ffd6572b94b779f30c46137af47eb0068c722c4
Jaela Wesley <jcwesley93@gmail.com>
2019-02-19 12:28:04 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+ def my_select(selection)
+  counter = 0
+  modified_array = []
+  while counter < selection.length
+    if yield(selection[counter])
+    modified_array << selection[counter]
+    counter += 1
+  end
+  modified_array
+end
 end
